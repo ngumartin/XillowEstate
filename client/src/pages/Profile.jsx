a27d7307fecf155aec5869dafa258b22154379d0
Martin Nguyen <ngumartin@gmail.com>
2023-11-17 10:35:27 -0800

    Create listing button and start on building create listing page.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,23 +1,12 @@
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import { useRef, useState, useEffect } from "react";
-import { 
-  getDownloadURL, 
-  getStorage, 
-  ref, 
-  uploadBytesResumable 
-} from 'firebase/storage';
-import { app } from "../firebase";
-import { 
-  updateUserStart, 
-  updateUserSuccess, 
-  updateUserFailure, 
-  deleteUserFailure, 
-  deleteUserStart, 
-  deleteUserSuccess, 
-  signOutUserFailure,
-  signOutUserStart
-} from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { Link } from 'react-router-dom';
+
+import { app } from "../firebase";
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice";
+
 export default function Profile() {
   const fileRef = useRef(null);
   const {currentUser, loading, error} = useSelector((state) => state.user);
@@ -179,6 +168,9 @@ export default function Profile() {
           className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80 disabled:opacity-65">
             { loading ? 'Loading...' : 'Update' }
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-80' to={'/create-listing'}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer border p-3 rounded-lg">Delete Account</span>
