f030b8d30ca9abaaa2d46a92caef0777ba342659
Martin Nguyen <ngumartin@gmail.com>
2023-11-20 23:07:08 -0800

    Progress on choosing image files to upload.

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,44 @@
-import React from 'react'
+import { 
+    getDownloadURL, 
+    getStorage, 
+    uploadBytesResumable 
+} from 'firebase/storage';
+import { useState } from 'react';
+
+import { app } from '../firebase';
 
 export default function CreateListing() {
+    const [files, setFiles] = useState([])
+    const handleImageSubmit = (e) => {
+        if (files.length > 0 && files.length < 7) {
+            const promises = [];
+            
+            for (let i = 0; i < files.length; i++) {
+                promises.push(storeImage(files[i]));
+            } 
+        }
+    }
+
+    const storeImage = async (file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                'state_changed',
+                (error)=>{
+                    reject(error);
+                },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                    });
+                }
+            );
+        });
+    };
+
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>
@@ -115,8 +153,19 @@ export default function CreateListing() {
                         <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6)</span>
                     </p>
                     <div className='flex gap-4'>
-                        <input className='p-3 border border-gray-400 rounded w-full' type="file" id='images' accept='image/*' multiple />
-                        <button className='p-3 font-semibold text-green-800 border border-green-800 rounded uppercase hover:bg-gray-300 disabled:opacity-80'>Upload</button>
+                        <input 
+                            onChange={(e)=>setFiles(e.target.files)} 
+                            className='p-3 border border-gray-400 rounded w-full' type="file" 
+                            id='images' 
+                            accept='image/*' 
+                            multiple 
+                        />
+                        <button 
+                            type="button" 
+                            onClick={handleImageSubmit} 
+                            className='p-3 font-semibold text-green-800 border border-green-800 rounded uppercase hover:bg-gray-300 disabled:opacity-80'>
+                                Upload
+                            </button>
                     </div>
                     <button className='p-3 bg-slate-600 text-white rounded-lg uppercase hover:opacity-80 disabled:opacity-40'>Create Listing</button>
                 </div>
