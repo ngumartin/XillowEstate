6f77805ca841251796b63f16176135dca9d3f5e8
Martin Nguyen <ngumartin@gmail.com>
2023-11-29 13:06:37 -0800

    Input forms, checkboxes, value prices, images, data fetched to create listing, in progress.

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -2,25 +2,29 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { useState } from 'react';
 
 import { app } from '../firebase';
+import {useSelector} from 'react-redux';
 
 export default function CreateListing() {
-    const [files, setFiles] = useState([])
+    const {currentUser} = useSelector(state => state.user);
+    const [files, setFiles] = useState([]);
     const [formData, setFormData] = useState({
         imageUrls: [],
-            name: '',
+        name: '',
             description: '',
             address: '',
             type: 'rent',
-            bedrooms: '0',
-            bathrooms: '0',
+            bedrooms: '1',
+            bathrooms: '1',
             regularPrice: 0,
             discountPrice: 0,
-            offer: 'false',
-            parking: 'false',
-            furnished: 'false',
+            offer: false,
+            parking: false,
+            furnished: false,
     });
     const [imageUploadError, setImageUploadError] = useState(false);
     const [uploading, setUploading] = useState(false);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
     console.log(formData);
     const handleImageSubmit = (e) => {
         if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
@@ -81,20 +85,69 @@ export default function CreateListing() {
     };
 
     const handleChange = (e) => {
-        if(e.target.id === 'sale' || e.target.id === 'rent') {
+
+        if (e.target.id === 'sale' || e.target.id === 'rent') {
             setFormData({
                 ...formData,
-                    type: e.target.id
+                type: e.target.id,
             })
+        };
+
+        if (
+            e.target.id === 'parking' || 
+            e.target.id === 'furnished' || 
+            e.target.id === 'offer'
+        ) {
+            setFormData({
+                ...formData,
+                [e.target.id]: e.target.checked
+            })
+        };
+
+        if (
+            e.target.type === 'number' ||
+            e.target.type === 'text' ||
+            e.target.type === 'textarea'
+        ) {
+            setFormData({
+                ...formData,
+                [e.target.id]: e.target.value,
+            });
         }
     };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true)
+            setError(false)
+            const res = await fetch('/api/listing/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                },
+                body: JSON.stringify({
+                    ...formData,
+                    userRef: currentUser._id,
+                }),
+            });
+            const data = await res.json();
+            setLoading(false);
+            if (data.success === false) {
+                setError(data.message)
+            }
+        } catch (error) {
+            setError(error.message)
+            setLoading(false)
+        }
+    }
 
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>
                 Create a Listing
             </h1>
-            <form className='flex flex-col sm:flex-row gap-6'>
+            <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-6'>
                 <div className='flex flex-col gap-4 flex-1'>
                     <input 
                         type="text" 
@@ -175,7 +228,7 @@ export default function CreateListing() {
                                 onChange={handleChange}
                                 checked={formData.offer}
                             />
-                            <span>Sale Pending</span>
+                            <span>Offer</span>
                         </div>
                     </div>
                     <div className='flex flex-wrap gap-6'>
@@ -283,7 +336,10 @@ export default function CreateListing() {
                             </div>
                         ))
                     }
-                    <button className='p-3 bg-slate-600 text-white rounded-lg uppercase hover:opacity-80 disabled:opacity-40'>Create Listing</button>
+                    <button className='p-3 bg-slate-600 text-white rounded-lg uppercase hover:opacity-80 disabled:opacity-40'>
+                        {loading ? 'Creating...' : 'Create Listing'}
+                    </button>
+                    {error && <p className='text-red-700 text-sm'>{error}</p>}
                 </div>
             </form>
         </main>
