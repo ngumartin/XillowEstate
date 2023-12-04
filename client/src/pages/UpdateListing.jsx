eb92bc3270b12f2552f205c9d2554c600ee002e2
Martin Nguyen <ngumartin@gmail.com>
2023-12-04 10:56:25 -0800

    Create api route for get listingId for update listing page.

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -31,8 +31,15 @@ export default function CreateListing() {
 
     useEffect(() => {
             const fetchListing = async () => {
-
-            }
+                const listingId = params.listingId;
+                const res = await fetch(`/api/listing/get/${listingId}`);
+                const data = await res.json();
+                if (data.success === false) {
+                    console.log(data.message);
+                    return;
+                };
+                setFormData(data);
+            };
             fetchListing();
         }, []);
     
