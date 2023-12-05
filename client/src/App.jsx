1185060d6c619f691caf3da83c75c11e820b220e
Martin Nguyen <ngumartin@gmail.com>
2023-12-05 11:06:15 -0800

    Append image to slider on listing page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,19 +15,21 @@ export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/listing/:listingId" element={<Listing />} />
-        <Route element={<PrivateRoute />} >  
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-listing" element={<CreateListing />} />
-          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
-        </Route>
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path='/listing/:listingId' element={<Listing />} />
+
+          <Route element={<PrivateRoute />} >  
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+            <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+          </Route>
         
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,9 +1,15 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css/bundle';
 
 export default function Listing() {
+    SwiperCore.use([Navigation]);
     const [listing, setListing] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const params = useParams();
     useEffect(() => {
@@ -11,7 +17,7 @@ export default function Listing() {
 
             try {
                 setLoading(true);
-                const res = await fetch(`/api/listing/get/${params.listingId}`)
+                const res = await fetch(`/api/listing/get/${params.listingId}`);
                 const data = await res.json();
                 if (data.success === false) {
                     setError(true);
@@ -19,14 +25,43 @@ export default function Listing() {
                     return;
                 }
                 setListing(data);
+                setLoading(false);
+                setError(false);
             } catch (error) {
                 setError(true);
-                setLoading(false);
+                setLoading(false); 
             }
         };
         fetchListing();
-    });
+    }, [params.listingId]);
+    console.log(loading)
+    console.log(listing)
+
     return (
-    <div>{listing && listing.name}</div>
+        <main>
+            {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+            {error && (
+                <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+            )}
+            {listing && !loading && !error && (
+                <div>
+                    <Swiper navigation>
+                        {listing.imageUrls.map((url) => (
+                            <SwiperSlide key={url}>
+                                <div
+                                    className='h-[550px]'
+                                    style={{
+                                        background: `url(${url}) center no-repeat`,
+                                        backgroundSize: 'cover',
+                                    }}
+                                >
+                                    <img src={url} alt="" />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            )};
+        </main>
     );
 };
