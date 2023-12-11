7a857a88d27029a5317b35ebf5b598dd9c8ed151
Martin Nguyen <ngumartin@gmail.com>
2023-12-11 12:56:13 -0800

    Add image slider to listings.

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
@@ -52,9 +52,8 @@ export default function Listing() {
                                     className='h-[550px]'
                                     style={{
                                         background: `url(${url}) center no-repeat`,
-                                        backgroundSize: 'cover',
-                                    }}
-                                >
+                                        backgroundSize: 'cover'
+                                }}>
                                     <img src={url} alt="" />
                                 </div>
                             </SwiperSlide>
