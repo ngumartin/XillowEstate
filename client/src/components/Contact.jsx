d5ec4ba31c3f18aae518381bda6e1e440e8ce404
Martin Nguyen <ngumartin@gmail.com>
2023-12-14 12:34:16 -0800

    Progress on contact lanlord button, routing and fetching.

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
 export default function Contact({listing}) {
+    const [landlord, setLandlord] = useState(null); 
+
+    useEffect(() => {
+        const fetchLandlord = async () => {
+            try {
+                const res = await fetch(`/api/user/${listing.userRef}`);
+                const data = await res.json();
+                setLandlord(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchLandlord();
+
+    }, [listing.userRef])
     return (
-    <div>Contact</div>
+    <div>
+        {landlord && (
+            <div className=''>
+                <p>Contact <span>{landlord.username}</span> for <span>{listing.name.toLowerCase}</span> </p>
+            </div>
+        )}
+    </div>
     )
 }
