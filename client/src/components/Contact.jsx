5d4cdca680a5cc3e16779ae5fafe92cd671565b8
Martin Nguyen <ngumartin@gmail.com>
2023-12-16 11:25:41 -0800

    Create send message button and styling, in progress.

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Contact({listing}) {
     const [landlord, setLandlord] = useState(null); 
@@ -24,10 +25,11 @@ export default function Contact({listing}) {
     return (
     <div>
         {landlord && (
-            <div className=''>
-                <p>Contact 
-                    <span className='font-semibold'>{landlord.username}</span> 
-                        for 
+            <div className='flex flex-col gap-2'>
+                <p>
+                    Contact {' '}
+                    <span className='font-semibold'>{landlord.username}</span>{' '}
+                        for {' '} 
                     <span className='font-semibold'>{listing.name.toLowerCase()}</span>
                 </p>
                 <textarea 
@@ -35,8 +37,17 @@ export default function Contact({listing}) {
                     id="message" 
                     rows="2" 
                     value={message} 
-                    onChange={onChange}>
+                    onChange={onChange}
+                    placeholder='Enter your message here...'
+                    className='w-full border p-3 rounded-lg'
+                    >
                 </textarea>
+                <Link 
+                    to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+                    className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg'
+                    >
+                    Send Message
+                </Link>
             </div>
         )}
     </div>
