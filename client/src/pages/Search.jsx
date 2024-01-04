96622fff4a6832107b006e8bc050d87056c199dd
Martin Nguyen <ngumartin@gmail.com>
2024-01-04 12:28:49 -0800

    Progress on search page input form and styling.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -4,8 +4,18 @@ export default function Search() {
     return (
     <div>
         <div className=''>
-            <form></form>
+            <form>
+                <div className='flex items-center gap-2'>
+                    <label className='whitespace-nowrap'>Search Term:</label>
+                    <input type="text" 
+                        id='searchTerm'
+                        placeholder='Search...'
+                        className='border rounded-lg p-3 w-full'
+                    />
+                </div>
+            </form>
         </div>
+
         <div className=''>
             <h1>Listing results:</h1>
         </div>
