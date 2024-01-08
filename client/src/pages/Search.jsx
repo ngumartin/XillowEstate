d9045dd65ec8bee395beef5329ee524de6591f4f
Martin Nguyen <ngumartin@gmail.com>
2024-01-08 11:25:28 -0800

    Progress on search page, styling.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Search() {
     return (
-    <div>
-        <div className=''>
+    <div className='flex flex-col md:flex-row'>
+        <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
             <form>
                 <div className='flex items-center gap-2'>
                     <label className='whitespace-nowrap'>Search Term:</label>
@@ -12,6 +12,9 @@ export default function Search() {
                         placeholder='Search...'
                         className='border rounded-lg p-3 w-full'
                     />
+                </div>
+                <div>
+                    <label>Type:</label>
                 </div>
             </form>
         </div>
