1ffa3950c22ddbfc65abfb7b25270ac74c95c6dc
Martin Nguyen <ngumartin@gmail.com>
2024-01-09 12:11:18 -0800

    Search page, progress on input functionalities.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,48 +1,91 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Search() {
+    const [sidebardata, setSidebardata] = useState({
+        searchTerm: '',
+        type: 'all',
+        parking: false,
+        furnished: false,
+        offer: false,
+        sort: 'created_at',
+        order: 'desc',
+    });
+
+    const handleChange = (e) => {};
+
     return (
     <div className='flex flex-col md:flex-row'>
         <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
             <form className='flex flex-col gap-8'>
+
                 <div className='flex items-center gap-2'>
                     <label className='whitespace-nowrap font-semibold'>Search Term:</label>
                     <input type="text" 
                         id='searchTerm'
                         placeholder='Search...'
                         className='border rounded-lg p-3 w-full'
+                        value={sidebardata.searchTerm}
+                        onChange={handleChange}
                     />
                 </div>
+
                 <div className='flex gap-2 flex-wrap items-center'>
                     <label className='font-semibold'>Type:</label>
+
                     <div className='flex gap-2'>
-                        <input type="checkbox" id='all' className='w-5' />
+                        <input type="checkbox" id='all' className='w-5' 
+                            onChange={handleChange}
+                            checked={sidebardata.type === 'all'}
+                        />
                         <span>Rent & Sale</span>
                     </div>
+
                     <div className='flex gap-2'>
-                        <input type="checkbox" id='rent' className='w-5' />
+                        <input type="checkbox" id='rent' className='w-5' 
+                            onChange={handleChange}
+                            checked={sidebardata.type === 'rent'}
+                        />
                         <span>Rent</span>
                     </div>
+
                     <div className='flex gap-2'>
-                        <input type="checkbox" id='sale' className='w-5' />
+                        <input type="checkbox" id='sale' className='w-5' 
+                            onChange={handleChange}
+                            checked={sidebardata.type === 'sale'}
+                        />
                         <span>Sale</span>
                     </div>
+
                     <div className='flex gap-2'>
-                        <input type="checkbox" id='offer' className='w-5' />
+                        <input type="checkbox" id='offer' className='w-5' 
+                            onChange={handleChange}
+                            checked={sidebardata.offer}
+                        />
                         <span>Offer</span>
                     </div>
                 </div>
+
                 <div className='flex gap-2 flex-wrap items-center'>
                     <label className='font-semibold'>Amenities:</label>
+
                     <div className='flex gap-2'>
-                        <input type="checkbox" id='parking' className='w-5' />
+                        <input type="checkbox" id='parking' className='w-5'
+                            onChange={handleChange}
+                            checked={sidebardata.parking}
+                        />
                         <span>Parking</span>
                     </div>
+
                     <div className='flex gap-2'>
-                        <input type="checkbox" id='furnished' className='w-5' />
+                        <input type="checkbox" id='furnished' className='w-5' 
+                            onChange={handleChange}
+                            checked={sidebardata.furnished}
+                        />
                         <span>Furnished</span>
                     </div>
                 </div>
+
                 <div className='flex items-center gap-2'>
                     <label className='font-semibold'>Sort:</label>
                     <select id="sort_order" className='border rounded-lg p-3'>
