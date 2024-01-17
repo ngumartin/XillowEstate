f33c10c2a1e84c458bb02f039314d86c14f32a14
Martin Nguyen <ngumartin@gmail.com>
2024-01-17 12:25:48 -0800

    Bug caught, listing Item page in progress, styling and functionality.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -13,7 +13,7 @@ export default function Search() {
         offer: false,
         sort: 'created_at',
         order: 'desc',
-    });
+});
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
@@ -206,14 +206,39 @@ export default function Search() {
             </form>
         </div>
 
-        <div className=''>
-            <h1 className='text-3xl font-semibold border-b p-3 mt-5 text-slate-700'>Listing results:</h1>
-            <div className=''>
+        <div className='flex-1'>
+            <h1 className='text-3xl font-semibold border-b p-7 mt-5 text-slate-700'>Listing results:
+            </h1>
+            <div className='p-7 flex flex-wrap gap-4'>
                 {!loading && listings.length === 0 && (
                     <p className='text-xl text-slate-700'>No listing found!</p>
                 )}
+                {loading && (
+                    <p className='text-xl text-slate-700 text-center w-full'>loading...</p>
+                )}
+                {
+                    !loading && listings && listings.map((listing) => (
+                        <ListingItem key={listing._id} listing={listing} />
+                    ))
+                }
             </div>
+
+            
         </div>
     </div>
     )
 }
+
+{/* <div className='p-7 flex flex-wrap gap-4'>
+                {!loading && listings.length === 0 && (
+                    <p className='text-xl text-slate-700'>No listing found!</p>
+                )}
+                {loading && (
+                    <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                )}
+                {!loading &&
+                    listings &&
+                    listings.map((listing) => (
+                        <ListingItem key={listing._id} listing={listing} />
+                ))}
+            </div> */}
