ae910cc67d31f633bfea03941cd443cecdafc54b
Martin Nguyen <ngumartin@gmail.com>
2024-01-19 15:47:29 -0800

    Complete about page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -48,7 +48,6 @@ export default function Home() {
   return (
     <div>
       {/* top */}
-
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
           Find your next <span className='text-slate-500'>perfect</span>
@@ -70,24 +69,21 @@ export default function Home() {
       </div>
 
       {/* swiper */}
-
       <Swiper navigation>
         {offerListings &&
           offerListings.length > 0 &&
-          offerListings.map((listing) => {
-            console.log(listing.imageUrls[0]); // Log the image URL
-            return (
-              <SwiperSlide key={listing._id}>
-                <div
-                  style={{
-                    background: `url(${listing.imageUrls[0]}) center/cover no-repeat`,
-                    height: '500px',
-                  }}
-                ></div>
-              </SwiperSlide>
-            );
-        })}
-
+          offerListings.map((listing) => (
+            <SwiperSlide>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='h-[500px]'
+                key={listing._id}
+              ></div>
+            </SwiperSlide>
+          ))}
       </Swiper>
 
       {/* listing results for offer, sale and rent */}
