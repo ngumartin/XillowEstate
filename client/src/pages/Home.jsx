54258103244db7db66d7a561d0a9b04ace7a0190
Martin Nguyen <ngumartin@gmail.com>
2024-02-14 11:10:45 -0800

    Fix listing issue

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
 import ListingItem from '../components/ListingItem';
 
+
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
